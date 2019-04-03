7f04c57c6f6f7194b15e7e755121e87797733c15
王嘉宁 <wangjianing@imdada.cn>
2019-04-03 10:15:54 +0800

    add：新建列表随机生成文件

---
+++ Arborea/RandomList/ViewController/RandomListListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RandomListListViewController.swift
+//  Arborea
+//
+//  Created by 王嘉宁 on 2019/4/3.
+//  Copyright © 2019 Johnny. All rights reserved.
+//
+
+import UIKit
+
+class RandomListListViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
